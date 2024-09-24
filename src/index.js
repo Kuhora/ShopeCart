1c9622f72e0c72b23b41c680137295d4e0e6ed06
kuhora <kuhora123@gmail.com>
2024-09-24 15:52:57 -0300

    Final

--- src/index.js
+++ src/index.js
@@ -12,9 +12,12 @@ const item2 = await createItem("hothwheels lamborghini", 39.99, 3);
 
 
 await cartService.addItem(myCart, item1)
-await cartService.addItem(myWishlist, item2)
+await cartService.addItem(myCart, item2)
+await cartService.displaycart(myCart);
 
-console.log("Shopee Cart TOTAL IS:");
+//await cartService.deleteItem(myCart, item1.name);
+//await cartService.deleteItem(myCart, item2.name);
+
 await cartService.calculateTotal(myCart);
 
 
--- src/services/cart.js
+++ src/services/cart.js
@@ -3,17 +3,46 @@ async function addItem(userCart, item){
 }
 
 async function calculateTotal(userCart){
+    console.log(" \nShopee Cart TOTAL IS:");
+
     const result = userCart.reduce((total, item) => total + item.subtotal(), 0);
-    console.log(result);
+    console.log(`\n🎁Total: ${result}`);
 }
 
 
 async function deleteItem(userCart, name){
+    const index = userCart.findIndex((item) => item.name === name);
 
+    if(index !== - 1){
+        userCart.splice(index, 1);
+    }
 }
 
 async function removeitem(userCart, index){
 
+    const indexFound = userCart.findIndex((p) => p.name === item.name);
+
+    if (indexFound == -1) {
+    console.log("item não encontrado");
+    return;
+    }
+
+    if (userCart[indexFound].quantity > 1) {
+    userCart[indexFound].quantity -= 1;
+    return;
+    }
+
+    if (userCart[indexFound].quantity == 1) {
+    userCart.splice(indexFound, 1);
+    return;
+    }
+}
+
+async function displaycart(userCart){
+    console.log("Shopee cart list:")
+    userCart.forEach((item, index) => {
+        console.log(`${index + 1}. ${item.name} - R$ ${item.price} | ${item.quantity}x | Subtotal = ${item.subtotal()}`);
+    });
 }
 
 export {
@@ -21,4 +50,5 @@ export {
     calculateTotal,
     deleteItem,
     removeitem,
+    displaycart, 
 }
